f9803caf3dd46789b7ba6a67de1fc8ba7e0fecab
mbjay89 <mbsiraj.89@hotmail.com>
2022-11-22 15:43:09 -0800

    Task 0

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *_printf - funciton that mimics printf from stdio
+  *@format: string to print 
+  *@...: variadic parameters
+  *
+  *Return: number of printed characters
+  */
+int _printf(const char *format, ...)
+{
+
+}
